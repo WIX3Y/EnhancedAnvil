3a6c31b91d8a2d08a33c5edc551fb0185a2dee55
WIX3Y <wikstromdenice@gmail.com>
2025-09-13 11:52:00 +0200

    rune support

--- src/main/java/wix3y/enhancedAnvil/handlers/PlayerEnchantHandler.java
+++ src/main/java/wix3y/enhancedAnvil/handlers/PlayerEnchantHandler.java
@@ -19,9 +19,7 @@ import java.util.Arrays;
 
 public class PlayerEnchantHandler implements Listener {
     private final EnhancedAnvil plugin;
-    private final int lowTierExpLevelCost = 10;
-    private final int mediumTierExpLevelCost = 30;
-    private final int highTierExpLevelCost = 50;
+    private final int expLevelCost = 10;
 
     public PlayerEnchantHandler(EnhancedAnvil plugin) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
@@ -33,7 +31,7 @@ public class PlayerEnchantHandler implements Listener {
      *
      * @param event the prepare item enchant event
      */
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerEnchant(PrepareItemEnchantEvent event) {
         for (int i=0; i<event.getOffers().length; i++) {
             if (i == 0 && !event.getEnchanter().hasPermission("enhancedanvil.enchant.low")) {
@@ -57,22 +55,27 @@ public class PlayerEnchantHandler implements Listener {
     @EventHandler(priority = EventPriority.LOW)
     public void onPlayerPrepareRuneEnchant(PrepareItemEnchantEvent event) {
         ItemStack item = event.getItem();
-        if (item.getType() == Material.AMETHYST_SHARD && item.hasItemMeta()) {
+        if (item.hasItemMeta()) {
             ItemMeta meta = item.getItemMeta();
-            if (!meta.hasEnchants() && meta.hasItemModel() && meta.getItemModel().equals(new NamespacedKey("enhancedanvil", "runestone"))) {
+            if (!meta.hasEnchants() && meta.hasItemModel() && (
+                    meta.getItemModel().equals(new NamespacedKey("enhancedanvil", "rune")) ||
+                    meta.getItemModel().equals(new NamespacedKey("enhancedanvil", "enchanted_rune_low")) ||
+                    meta.getItemModel().equals(new NamespacedKey("enhancedanvil", "enchanted_rune_medium")) ||
+                    meta.getItemModel().equals(new NamespacedKey("enhancedanvil", "enchanted_rune_high"))
+            )) {
 
                 Arrays.fill(event.getOffers(), null);
                 int numbBookshelves = event.getEnchantmentBonus();
                 Player player = event.getEnchanter();
 
                 if (numbBookshelves >= 0 && player.hasPermission("enhancedanvil.enchant.low")) {
-                    event.getOffers()[0] = new EnchantmentOffer(Enchantment.UNBREAKING, 1, lowTierExpLevelCost);
+                    event.getOffers()[0] = new EnchantmentOffer(Enchantment.UNBREAKING, 1, expLevelCost);
                 }
                 if (numbBookshelves >= 8 && player.hasPermission("enhancedanvil.enchant.medium")) {
-                    event.getOffers()[1] = new EnchantmentOffer(Enchantment.UNBREAKING, 2, mediumTierExpLevelCost);
+                    event.getOffers()[1] = new EnchantmentOffer(Enchantment.UNBREAKING, 1, expLevelCost);
                 }
                 if (numbBookshelves >= 15 && player.hasPermission("enhancedanvil.enchant.high")) {
-                    event.getOffers()[2] = new EnchantmentOffer(Enchantment.UNBREAKING, 3, highTierExpLevelCost);
+                    event.getOffers()[2] = new EnchantmentOffer(Enchantment.UNBREAKING, 1, expLevelCost);
                 }
             }
         }
@@ -86,22 +89,21 @@ public class PlayerEnchantHandler implements Listener {
     @EventHandler
     public void onPlayerRuneEnchant(EnchantItemEvent event) {
         ItemStack item = event.getItem();
-        if (item.getType() == Material.AMETHYST_SHARD && item.hasItemMeta()) {
+        if (item.hasItemMeta()) {
             ItemMeta meta = item.getItemMeta();
-            if (!meta.hasEnchants() && meta.hasItemModel() && meta.getItemModel().equals(new NamespacedKey("enhancedanvil", "runestone"))) {
+
+            if (!meta.hasEnchants() && meta.hasItemModel() && (
+                    meta.getItemModel().equals(new NamespacedKey("enhancedanvil", "rune")) ||
+                    meta.getItemModel().equals(new NamespacedKey("enhancedanvil", "enchanted_rune_low")) ||
+                    meta.getItemModel().equals(new NamespacedKey("enhancedanvil", "enchanted_rune_medium")) ||
+                    meta.getItemModel().equals(new NamespacedKey("enhancedanvil", "enchanted_rune_high"))
+            )) {
+
                 // apply enchants to rune
                 int cost = event.getExpLevelCost();
-                if (cost == lowTierExpLevelCost) {
+                if (cost == expLevelCost) {
                         event.getEnchantsToAdd().clear();
                         event.getEnchantsToAdd().put(Enchantment.UNBREAKING, 1);
-                }
-                else if (cost == mediumTierExpLevelCost) {
-                    event.getEnchantsToAdd().clear();
-                    event.getEnchantsToAdd().put(Enchantment.UNBREAKING, 2);
-                }
-                else if (cost == highTierExpLevelCost) {
-                    event.getEnchantsToAdd().clear();
-                    event.getEnchantsToAdd().put(Enchantment.UNBREAKING, 3);
                 }
                 else {
                     plugin.getLogger().warning("Exp level cost " + cost + " does not match any tier. Runestone will not be enchanted for player " + event.getEnchanter().getName() + ".");
